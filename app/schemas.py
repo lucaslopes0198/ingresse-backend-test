c38547224625a22b40662b9dc0a9df2df41d81c6
lucaslopes0198 <lucaslopes0198@gmail.com>
2020-01-31 14:17:03 -0300

    Close #6

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask_marshmallow import Marshmallow
+
+# app = Flask(__name__)
+ma = Marshmallow()
+
+# Datetime Schema
+class DatetimeSchema(ma.Schema):
+	class Meta:
+		fields = ['datetime']
+
+# Tag Schema
+class TagSchema(ma.Schema):
+	class Meta:
+		fields = ['tag']
+
+# Event Schema
+class EventSchema(ma.Schema):
+	class Meta:
+		fields = ['id', 'name', 'place', 'interested', 'tags', 'datetimes']
+	tags = ma.Pluck(TagSchema, 'tag', many=True)
+	datetimes = ma.Pluck(DatetimeSchema, 'datetime', many=True)
+
+# Init schema
+event_schema = EventSchema()
+events_schema = EventSchema(many=True)
